2726ac571fdc7e04a29c054f121743cc6c0aa386
Kevin Duff <kevinkelduff@gmail.com>
2021-10-18 00:21:24 +0100

    Fix baseURL issue with axios

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,11 @@
-const OSRS_ITEMS_API_URL = process.env.REACT_APP_OSRS_ITEMS_API_URL ?? "";
+export const OSRS_ITEMS_API_URL =
+  process.env.REACT_APP_OSRS_ITEMS_API_URL ?? "";
 
-export const ITEM_ENDPOINT = `${OSRS_ITEMS_API_URL}/item/`;
-export const ITEMS_ENDPOINT = `${OSRS_ITEMS_API_URL}/items/`;
-export const TAG_ENDPOINT = `${OSRS_ITEMS_API_URL}/tag/`;
-export const TAGS_ENDPOINT = `${OSRS_ITEMS_API_URL}/tags/`;
-export const TAG_GROUPS_ENDPOINT = `${OSRS_ITEMS_API_URL}/tagGroups/`;
-export const RELATED_ITEMS_ENDPOINT = `${ITEMS_ENDPOINT}related/`;
-export const ITEMS_BY_TAG_ENDPOINT = `${ITEMS_ENDPOINT}tag/`;
-export const TAGS_BY_ITEM_ENDPOINT = `${TAGS_ENDPOINT}item/`;
+export const ITEM_ENDPOINT = `item`;
+export const ITEMS_ENDPOINT = `items`;
+export const TAG_ENDPOINT = `tag`;
+export const TAGS_ENDPOINT = `tags`;
+export const TAG_GROUPS_ENDPOINT = `tagGroups/`;
+export const RELATED_ITEMS_ENDPOINT = `${ITEMS_ENDPOINT}/related/`;
+export const ITEMS_BY_TAG_ENDPOINT = `${ITEMS_ENDPOINT}/tag/`;
+export const TAGS_BY_ITEM_ENDPOINT = `${TAGS_ENDPOINT}/item/`;
--- src/hooks/api.ts
+++ src/hooks/api.ts
@@ -2,6 +2,7 @@ import useAxios from "axios-hooks";
 import {
   ITEMS_BY_TAG_ENDPOINT,
   ITEMS_ENDPOINT,
+  OSRS_ITEMS_API_URL,
   TAGS_BY_ITEM_ENDPOINT,
   TAGS_ENDPOINT,
   TAG_GROUPS_ENDPOINT,
@@ -17,30 +18,38 @@ export const useGetItems = (params: {
 }) =>
   useAxios<ItemsSearchResult>({
     url: ITEMS_ENDPOINT,
+    baseURL: OSRS_ITEMS_API_URL,
     params,
   });
 
 export const useGetTagGroups = (params: { nameLike?: string }) =>
   useAxios<string[]>({
     url: TAG_GROUPS_ENDPOINT,
+    baseURL: OSRS_ITEMS_API_URL,
     params,
   });
 
 export const useGetItemTags = (itemId: number) =>
   useAxios<OsrsTag[]>({
     url: TAGS_BY_ITEM_ENDPOINT + `${itemId}`,
+    baseURL: OSRS_ITEMS_API_URL,
   });
 
 export const useGetItemsByTag = (
   tagName: string,
   params: { includeRelated?: boolean }
 ) =>
-  useAxios<OsrsItem[]>({ url: ITEMS_BY_TAG_ENDPOINT + `${tagName}`, params });
+  useAxios<OsrsItem[]>({
+    url: ITEMS_BY_TAG_ENDPOINT + `${tagName}`,
+    baseURL: OSRS_ITEMS_API_URL,
+    params,
+  });
 
 export const useCreateItemTags = () =>
   useAxios<OsrsTag[]>(
     {
       url: TAGS_ENDPOINT,
+      baseURL: OSRS_ITEMS_API_URL,
       method: "POST",
       params: {},
     },
@@ -51,6 +60,7 @@ export const useDeleteItemTags = () =>
   useAxios<OsrsTag[]>(
     {
       url: TAGS_ENDPOINT,
+      baseURL: OSRS_ITEMS_API_URL,
       method: "DELETE",
       params: {},
     },
